32f66cad082f13556858e25635ab484ab824695a
JacoboUribe28 <jacobouribemontoya@gmail.com>
2025-05-15 17:03:44 -0500

    enrutamientos

--- rappi/src/routes/index.ts
+++ rappi/src/routes/index.ts
@@ -10,12 +10,78 @@ const Tables = lazy(() => import('../pages/Tables'));
 const Alerts = lazy(() => import('../pages/UiElements/Alerts'));
 const Buttons = lazy(() => import('../pages/UiElements/Buttons'));
 const ListUsers= lazy(() => import('../pages/Users/page'))
+const ListAddress = lazy(() => import('../pages/Address/ListAddress'));
+const ListCustomer = lazy(() => import('../pages/Customer/ListCustomer'));
+const ListDriver = lazy(() => import('../pages/Driver/listDriver'));
+const ListIssue = lazy(() => import('../pages/Issue/listIssue'));
+const ListMenu = lazy(() => import('../pages/Menu/listMenu'));
+const ListMotorcycle = lazy(() => import('../pages/Motorcycle/listMotorcycle'));
+const ListOrder = lazy(() => import('../pages/Order/listOrder'));
+const ListRestaurant = lazy(() => import('../pages/Restaurant/listRestaurant'));
+const ListPhoto = lazy(() => import('../pages/Photo/listPhoto'));
+const ListProduct = lazy(() => import('../pages/Product/listProduct'));
+const ListShift = lazy(() => import('../pages/Shift/listShift'));
 
 const coreRoutes = [
   {
     path: '/list-users',
     title: 'ListUsers',
     component: ListUsers,
+  },
+  {
+    path: '/list-address',
+    title: 'ListAddress',
+    component: ListAddress,
+  },
+  {
+    path: '/list-customer',
+    title: 'ListCustomer',
+    component: ListCustomer,
+  },
+  {
+    path: '/list-driver',
+    title: 'ListDriver',
+    component: ListDriver,
+  },
+  {
+    path: '/list-issue',
+    title: 'ListIssue',
+    component: ListIssue,
+  },
+  {
+    path: '/list-menu',
+    title: 'ListMenu',
+    component: ListMenu,
+  },
+  {
+    path: '/list-motorcycle',
+    title: 'ListMotorcycle',
+    component: ListMotorcycle,
+  },
+  {
+    path: '/list-order',
+    title: 'ListOrder',
+    component: ListOrder,
+  },
+  {
+    path: '/list-restaurant',
+    title: 'ListRestaurant',
+    component: ListRestaurant,
+  },
+  {
+    path: '/list-photo',
+    title: 'ListPhoto',
+    component: ListPhoto,
+  },
+  {
+    path: '/list-product',
+    title: 'ListProduct',
+    component: ListProduct,
+  },
+  {
+    path: '/list-shift',
+    title: 'ListShift',
+    component: ListShift,
   },
   {
     path: '/calendar',
